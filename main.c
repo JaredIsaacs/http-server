173780475fcf65f6470c41adbc5262acbfcfbb91
JaredIsaacs <jaredisaacs6@gmail.com>
2023-11-01 19:37:05 -0400

    feat: initialize Winsock

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <ws2tcpip.h>
+#include <winsock2.h>
+
+#pragma(lib, "Ws2_32.lib")
 
 int main(){
+    WSADATA wsaData;
+    int r;
+
+    // Initialize Winsock
+    r = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if(r != 0){
+        printf("WSAStartup failed: %d\n", r);
+        return -1;
+    }
 
     return -0;
 }
