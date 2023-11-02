90f833229a391e204989e2d200533f6530b60c19
JaredIsaacs <jaredisaacs6@gmail.com>
2023-11-01 20:21:48 -0400

    feat: create a socket for communication with the client

--- main.c
+++ main.c
@@ -4,7 +4,11 @@
 
 #pragma(lib, "Ws2_32.lib")
 
+#define HTTP_PORT "80"
+
 int main(){
+    struct addrinfo *result = NULL, hints;
+    SOCKET ListenSocket = INVALID_SOCKET;
     WSADATA wsaData;
     int r;
 
@@ -15,5 +19,29 @@ int main(){
         return -1;
     }
 
+    // Resolve the local address ad prot to be used by the server
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+    hints.ai_flags = AI_PASSIVE;
+
+    r = getaddrinfo(NULL, HTTP_PORT, &hints, &result);
+    if(r != 0){
+        printf("getaddrinfo failed: %d\n", r);
+        WSACleanup();
+        return -1;
+    }
+
+    // Create a socket for the server to listen for client connections
+    ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+    if(ListenSocket == INVALID_SOCKET){
+        printf("Error at socket(): %ld\n", WSAGetLastError());
+        freeaddrinfo(result);
+        WSACleanup();
+        return 1;
+    }
+
+    WSACleanup();
     return -0;
 }
